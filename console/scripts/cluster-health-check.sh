88eafae4228efbdd8e126e4d4ade823a0804827d
Trenton Potgieter <darkreapyre@gmail.com>
2019-02-15 13:39:27 -0800

    Moved console components to separate branch

--- console/scripts/cluster-health-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 $DEEPLEARNING_WORKERS_COUNT)
-do
-echo "Health check on worker: deeplearning-worker$i"
-ssh -oStrictHostKeyChecking=no deeplearning-worker$i "touch /efs/deeplearning-worker$i-health.check"
-done
