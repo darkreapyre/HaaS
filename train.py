a081f553c04bd08c3f598760551440b27a8cb839
Trenton Potgieter <darkreapyre@gmail.com>
2019-07-23 20:22:43 -0700

    Tweaks

--- train.py
+++ train.py
@@ -19,32 +19,19 @@ def load_data(data_path):
     # Returns
         Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     """
-    # dirname = os.path.join('datasets', 'fashion-mnist')
-    # base = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/'
     files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
              't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz']
-
     paths = []
     for fname in files:
-        paths.append(os.join(data_path, fname))
-        # paths.append(get_file(fname,
-        #                       origin=base + fname,
-        #                       cache_subdir=dirname))
-
+        paths.append(os.path.join(data_path, fname))
     with gzip.open(paths[0], 'rb') as lbpath:
         y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-
     with gzip.open(paths[1], 'rb') as imgpath:
-        x_train = np.frombuffer(imgpath.read(), np.uint8,
-                                offset=16).reshape(len(y_train), 28, 28)
-
+        x_train = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
     with gzip.open(paths[2], 'rb') as lbpath:
         y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-
     with gzip.open(paths[3], 'rb') as imgpath:
-        x_test = np.frombuffer(imgpath.read(), np.uint8,
-                               offset=16).reshape(len(y_test), 28, 28)
-
+        x_test = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
     return (x_train, y_train), (x_test, y_test)
 
 parser = argparse.ArgumentParser(description='Keras Fashion MNIST Example',
