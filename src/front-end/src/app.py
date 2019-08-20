cccffbf0003d53a461f92fae8ce15e913f6b0792
Trenton Potgieter <darkreapyre@gmail.com>
2019-08-20 09:55:42 -0700

    Cleanup

--- src/front-end/src/app.py
+++ src/front-end/src/app.py
@@ -51,65 +51,6 @@ def create_sm_experiment():
         s3.Object(S3_BUCKET_NAME, S3_OBJECT_NAME).put(Body=S3_OBJECT_JSON)
         return render_template('experiment_created.html', dashboard=os.environ['DASHBOARD_URL'], name=experiment['Model_Name'])
 
-@app.route('/ekscreate', methods=['GET', 'POST'])
-def create_eks_experiment():
-    form = EKSForm(request.form)
-    if not form.validate_on_submit():
-        return render_template('create_eks_experiment.html', form=form)
-    if request.method == 'POST':
-        S3_BUCKET_NAME = os.environ['S3_BUCKET']
-        print(S3_BUCKET_NAME) # Debug
-        S3_OBJECT_NAME = "experiment_config.json"
-        time_stamp = str(datetime.now()).split(' ')
-        date = time_stamp[0]
-        time = time_stamp[1].replace(':', '-').split('.')[0]
-        experiment = {}
-        experiment['Model_Name'] = request.form.get('Model_Name')
-        experiment['Version'] = request.form.get('Version')
-        experiment['Description'] = 'Experiment created on {} at {}.'.format(date, time)
-        experiment['Platform'] = 'eks'
-        experiment['Dataset_Name'] = request.form.get('Dataset_Name')
-        experiment['GitHub_User'] = request.form.get('Github_User')
-        experiment['GitHub_Repo'] = request.form.get('Github_Repo')
-        experiment['Dashboard_URL'] = os.environ['DASHBOARD_URL']
-        experiment['Training_Instance'] = request.form.get('Training_Instance')
-        experiment['Instance_Count'] = request.form.get('Instance_Count')
-        experiment['Hyperparameters'] = request.form.get('Hyperparameters')
-        S3_OBJECT_JSON = json.dumps(experiment)
-        s3 = boto3.resource('s3')
-        s3.Object(S3_BUCKET_NAME, S3_OBJECT_NAME).put(Body=S3_OBJECT_JSON)
-        return render_template('experiment_created.html', dashboard=os.environ['DASHBOARD_URL'], name=experiment['Model_Name'])
-
-
-@app.route('/amicreate', methods=['GET', 'POST'])
-def create_ami_experiment():
-    form = AMIForm(request.form)
-    if not form.validate_on_submit():
-        return render_template('create_ami_experiment.html', form=form)
-    if request.method == 'POST':
-        S3_BUCKET_NAME = os.environ['S3_BUCKET']
-        print(S3_BUCKET_NAME) # Debug
-        S3_OBJECT_NAME = "experiment_config.json"
-        time_stamp = str(datetime.now()).split(' ')
-        date = time_stamp[0]
-        time = time_stamp[1].replace(':', '-').split('.')[0]
-        experiment = {}
-        experiment['Model_Name'] = request.form.get('Model_Name')
-        experiment['Version'] = request.form.get('Version')
-        experiment['Description'] = 'Experiment created on {} at {}.'.format(date, time)
-        experiment['Platform'] = 'dlami'
-        experiment['Dataset_Name'] = request.form.get('Dataset_Name')
-        experiment['GitHub_User'] = request.form.get('Github_User')
-        experiment['GitHub_Repo'] = request.form.get('Github_Repo')
-        experiment['Dashboard_URL'] = os.environ['DASHBOARD_URL']
-        experiment['Training_Instance'] = request.form.get('Training_Instance')
-        experiment['Instance_Count'] = request.form.get('Instance_Count')
-        experiment['Hyperparameters'] = request.form.get('Hyperparameters')
-        S3_OBJECT_JSON = json.dumps(experiment)
-        s3 = boto3.resource('s3')
-        s3.Object(S3_BUCKET_NAME, S3_OBJECT_NAME).put(Body=S3_OBJECT_JSON)
-        return render_template('experiment_created.html', dashboard=os.environ['DASHBOARD_URL'], name=experiment['Model_Name'])
-
 # Note: The following can be used for multi-user experiments
 # @app.route('/user/<user_date>/<user_key>')
 # def show_user_data(user_date,user_key):
