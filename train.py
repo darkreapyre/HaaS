8e7801604395840548251bf339e687b7ad6458a8
Trenton Potgieter <darkreapyre@gmail.com>
2019-09-06 16:39:29 -0700

    Resetting no. workers

--- train.py
+++ train.py
@@ -200,13 +200,13 @@ model.fit_generator(train_iter,
                     callbacks=callbacks,
                     epochs=args.epochs,
                     verbose=verbose,
-                    workers=4,
+                    workers=1,
                     initial_epoch=resume_from_epoch,
                     validation_data=test_iter,
                     validation_steps=3 * len(test_iter) // hvd.size())
 
 # Evaluate the model on the full data set.
-score = model.evaluate_generator(test_iter, len(test_iter), workers=4)
+score = model.evaluate_generator(test_iter, len(test_iter), workers=1)
 if verbose:
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
